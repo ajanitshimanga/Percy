ea04830b9912d8186699ad574441c6d32d2a0709
Eren-Ajani Tshimanga <eren.tish@gmail.com>
2024-09-25 17:50:53 -0700

    feat: UserModel and CharacterModel defined for postgres tables for metadata store.

---
+++ percy/metadata_store.py
@@ -0,0 +1,61 @@
+from sqlalchemy import String, JSON, DateTime
+from sqlalchemy.dialects.postgresql import ARRAY
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.testing.pickleable import User
+from sqlalchemy.testing.schema import Column
+
+Base = declarative_base()
+
+
+class UserModel(Base):
+    __tablename__ = "users"
+    __table_args__ = {"extend_existing": True}
+
+    id = Column(String, primary_key=True)
+    username = Column(String, nullable=False)
+    created_at = Column(DateTime(timezone=True))
+
+    def __repr__(self) -> str:
+        return f"<User(id='{self.id}' username='{self.username}')>"
+
+    def to_record(self) -> User:
+        return User(id=self.id, username=self.username, created_at=self.created_at)
+
+
+# Define Character Model scheme for postgresql
+class CharacterModel(Base):
+    __tablename__ = "characters"
+    __table_args__ = {"extend_existing": True}
+
+    user_id = Column(String, primary_key=True, index=True)
+    character_id = Column(String, index=True)
+    character_name = Column(String)
+    lore = Column(String)
+    past = Column(String)
+    appearance = Column(String)
+    abilities = Column(JSON)
+
+    weakness = Column(String)
+    strengths = Column(String)
+    affiliations = Column(String)
+    phrases = Column(ARRAY(String))
+    interests = Column(ARRAY(String))
+    relationships = Column(JSON)
+
+    def __repr__(self):
+        return (
+            f"<Character("
+            f"user_id={self.user_id}, "
+            f"character_id={self.character_id}, "
+            f"character_name='{self.character_name}', "
+            f"lore='{self.lore}', "
+            f"past='{self.past}', "
+            f"appearance='{self.appearance}', "
+            f"abilities={self.abilities}, "
+            f"weakness='{self.weakness}', "
+            f"strengths='{self.strengths}', "
+            f"affiliations='{self.affiliations}', "
+            f"phrases={self.phrases}, "
+            f"interests={self.interests}, "
+            f"relationships={self.relationships})>"
+        )
