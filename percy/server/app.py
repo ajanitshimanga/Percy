21898ebd8bea76927bc365f3084e9718d4a06fe9
Eren-Ajani Tshimanga <eren.tish@gmail.com>
2024-10-18 17:57:48 -0700

    feat: pushing server details down into server and cleaned up app layer

--- percy/server/app.py
+++ percy/server/app.py
@@ -1,97 +1,74 @@
-import os
-import uuid
-
 from fastapi import FastAPI, Depends
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, Session
 
-#from percy.metadata_store import CharacterModel
 from percy.schemas.characters import CharacterCreateResponse, CharacterCreateRequest, CharacterDeleteResponse, \
-    CharacterGetRequest, CharacterGetResponse
+    CharacterGetRequest, CharacterGetResponse, CharacterUpdateRequest, CharacterUpdateResponse
 from percy.server.server import get_percy_server
 
 
-app = FastAPI()
+def create_application() -> FastAPI:
+    fast_api_app = FastAPI()
+
+    # TODO(ajanitshimanga): Add middleware and http header stubbing.
+
+    return fast_api_app
+
+
+# Initialize app
+app = create_application()
 
 
 # Create a new character
-# TODO(ajanitshimanga): Break out direct database use into a server instance that can make a rewuest to commit a char.
 @app.post("/characters/", response_model=CharacterCreateResponse)
-def create_character(character: CharacterCreateRequest, percy_server: Depends(get_percy_server)):
+def create_character(request: CharacterCreateRequest, percy_server: Depends(get_percy_server)):
+    character_id = request.character_id
+    name = request.name
+    lore = request.lore
+    appearance = request.appearance
+    misc = request.misc
 
-    character_id = character.character_id
-    name = character.name
-    lore = character.lore
-    appearance = character.appearance
-    misc = character.misc
+    response = percy_server.create_character(character_id=character_id,
+                                             character_name=name,
+                                             lore=lore,
+                                             appearance=appearance,
+                                             misc=misc)
 
-    percy_server.create_character(character_id=character_id,
-                                  character_name=name,
-                                  lore=lore,
-                                  appearance=appearance,
-                                  misc=misc)
-
-    return CharacterCreateResponse(character_id=character_id)
-
-
-# Get a character
-# TODO(ajanitshimanga): Break out direct database use into a server instance that can make a rewuest to commit a char.
-@app.post("/characters/", response_model=CharacterGetResponse)
-def get_character(request: CharacterGetRequest, percy_server: Depends(get_percy_server)):
-
-    response = percy_server.get_character(character_id=request.character_id, character_name=request.character_name)
     return response
 
 
+# Update a character
+@app.patch("/characters/{character_id}", response_model=CharacterUpdateResponse)
+def update_character(request: CharacterUpdateRequest, percy_server: Depends(get_percy_server)):
+    character_id = request.character_id
+    character_name = request.character_name
+    lore = request.lore
+    appearance = request.appearance
+    misc = request.misc
+
+    response = percy_server.update_character(character_id=character_id,
+                                             character_name=character_name,
+                                             lore=lore,
+                                             appearance=appearance,
+                                             misc=misc
+                                             )
+    return response
+
+
+# Get a character
+@app.get("/characters/", response_model=CharacterGetResponse)
+def get_character(request: CharacterGetRequest, percy_server: Depends(get_percy_server)):
+    character_id = request.character_id
+    character_name = request.character_name
+
+    response = percy_server.get_character(character_id=character_id,
+                                          character_name=character_name
+                                          )
+
+    return response
 
 
 # Delete a character
 @app.delete("/characters/{character_id}", response_model=CharacterDeleteResponse)
 def delete_character(character_id: str, percy_server: Depends(get_percy_server)):
+    response = percy_server.delete_character(character_id=character_id)
 
-    character_name = percy_server.delete_character(character_id=character_id)
-
-    return CharacterDeleteResponse(character_id=character_id, character_name=str(character_name))
-
-
-# TODO(ajanitshimanga): Add in other endpoints later.
-
-# # Read a character based on id
-# @app.get("/characters/{user_id}")
-# def get_character(character_read: CharacterGetRequest, db: Session = next(get_db())):
-
-
-
-# class CharacterReadRequest(BaseModel):
-#     id: UUID = str(uuid.uuid4())
-#
-#     class Config:
-#         orm_mode = True
-#
-#
-# class CharacterReadResponse(BaseModel):
-
-
-# TODO(ajanitshimanga): Add in other endpoints later.
-#
-# # Read all characters
-# @app.get("/characters/{user_id}", response_model=List[CharacterReadResponse])
-# def list_characters(user_id: UUID, skip: int = 0, limit: int = 10, db: Session = next(get_db())):
-#     return db.query(CharacterModel).offset(skip).limit(limit).all()
-#
-#
-# # Update a character
-# @app.put("/characters/{character_id}", response_model=CharacterReadResponse)
-# def update_character(character_id: UUID, character: CharacterCreateRequest, db: Session = Depends(get_db)):
-#     db_character = db.query(CharacterModel).filter(CharacterModel.id == character_id).first()
-#     if not db_character:
-#         raise HTTPException(status_code=404, detail="Character not found")
-#
-#     for key, value in character.dict().items():
-#         setattr(db_character, key, value)
-#
-#     db.commit()
-#     db.refresh(db_character)
-#     return db_character
-#
-#
+    return response
--- percy/server/server.py
+++ percy/server/server.py
@@ -1,14 +1,28 @@
 import os
 from abc import abstractmethod
-from http.client import HTTPException
 from typing import Optional
 
-from fastapi import Depends
+from fastapi import Depends, HTTPException
+from letta.client.client import AbstractClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session, sessionmaker
 
 from percy.metadata_store import CharacterModel
-from percy.schemas.characters import CharacterCreateRequest, CharacterGetResponse, CharacterCreateResponse
+from percy.schemas.characters import CharacterGetResponse, CharacterCreateResponse, \
+    CharacterDeleteResponse, CharacterUpdateResponse
+
+# TODO(ajanitshimanga): dockerize application and spin up postgres & letta servers.
+
+# Env variables
+POSTGRES_USER = os.getenv("POSTGRES_USER", "percy")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "percy-password")
+POSTGRES_DB = os.getenv("POSTGRES_DB", "percy-db")
+
+# Database setup
+# Update with your DB URL TODO(ajanitshimanga): Change to be db agnostic with a db provider factory.
+DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@localhost:5432/{POSTGRES_DB}"
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
 class AbstractPercyContext(object):
@@ -21,7 +35,7 @@ class AbstractPercyContext(object):
         raise NotImplementedError
 
     @abstractmethod
-    def update_character(self, character_id: str, character_name: str, character_dict: dict):
+    def update_character(self, character_id: str, character_name: str, lore: str, appearance: str, misc: str):
         raise NotImplementedError
 
     @abstractmethod
@@ -29,13 +43,13 @@ class AbstractPercyContext(object):
         raise NotImplementedError
 
     @abstractmethod
-    def list_characters(self):
+    def list_characters(self, user_id: str):
         raise NotImplementedError
 
 
 class PercyManagementContext(AbstractPercyContext):
 
-    # TODO(ajanitshimanga): Refactor db to a metadata service.
+    # TODO(ajanitshimanga): Refactor raw db to a metadata service.
     def __init__(self, db: Session, agent_client):
         self.db = db
         self.agent_client = agent_client
@@ -57,9 +71,12 @@ class PercyManagementContext(AbstractPercyContext):
         response = CharacterCreateResponse(character_id=character_id)
         return response
 
-    def get_character(self, character_id: str, character_name: str) -> CharacterGetResponse:
+    def get_character(self, character_id: str, character_name: Optional[str] = None) -> CharacterGetResponse:
         # Retrieve record
         character_record = self.db.query(CharacterModel).filter(CharacterModel.id == character_id).first()
+
+        if not character_record:
+            raise HTTPException(status_code=404, detail="Character not found")
 
         character_id = character_record.character_id
         character_name = character_record.name
@@ -72,34 +89,46 @@ class PercyManagementContext(AbstractPercyContext):
                                     character_age=character_age
                                     )
 
-    def update_character(self, character_id: str, character_name: str, character_create: CharacterCreateRequest):
+    def update_character(self, character_id: str, character_name: str, lore: str, appearance: str, misc: str) -> CharacterUpdateResponse:
+        # Retrieve record
         db_character = self.db.query(CharacterModel).filter(CharacterModel.id == character_id).first()
 
-    def delete_character(self, character_id: str):
-        # TODO: Default path
+        if not db_character:
+            raise HTTPException(status_code=404, detail="Character not found")
+
+        # Set attributes only if passed
+        if character_name:
+            db_character.character_name = character_name
+
+        if lore:
+            db_character.lore = lore
+
+        if appearance:
+            db_character.appearance = appearance
+
+        if misc:
+            db_character.misc = misc
+
+        self.db.commit()
+        self.db.refresh(db_character)   # We don't use the record after so isn't important.
+
+        return CharacterUpdateResponse(character_id)
+
+    def delete_character(self, character_id: str) -> CharacterDeleteResponse:
+        # TODO(ajanitshimanga): hide guts / direct db access to a DataAccess class rather than in server endpoints.
         db_character = self.db.query(CharacterModel).filter(CharacterModel.id == character_id).first()
         if not db_character:
             raise HTTPException(status_code=404, detail="Character not found")
 
         self.db.delete(db_character)
         self.db.commit()
-        return db_character
+
+        CharacterDeleteResponse(character_id=character_id, character_name=str(character_name))
+        return
 
     def list_characters(self):
         # TODO: Default path
         raise NotImplementedError
-
-
-# Env variables
-POSTGRES_USER = os.getenv("POSTGRES_USER", "percy")
-POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "percy-password")
-POSTGRES_DB = os.getenv("POSTGRES_DB", "percy-db")
-
-# Database setup
-# Update with your DB URL TODO(ajanitshimanga): Change to be db agnostic with a db provider factory.
-DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@localhost:5432/{POSTGRES_DB}"
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
 # Database dependency for FastAPI
@@ -111,6 +140,15 @@ def get_db():
         db.close()
 
 
+# Letta dependency for agents
+def get_letta_client():
+    from letta import create_client
+
+    letta_client = create_client()
+    return letta_client
+
+
 # Service instance using the stubbed database
-def get_percy_server(db: Session = Depends(get_db)) -> PercyManagementContext:
-    return PercyManagementContext(db)
+def get_percy_server(db: Session = Depends(get_db),
+                     letta_client: AbstractClient = Depends(get_letta_client())) -> PercyManagementContext:
+    return PercyManagementContext(db, letta_client)
